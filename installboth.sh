e3298ee1ec7415d43831024e8be865f020f91458
olafur-palsson <olafur.palsson2@gmail.com>
2018-09-12 20:46:53 +0000

    Butfix a installer

--- installboth.sh
+++ installboth.sh
@@ -1,16 +1,18 @@
 
 currentFolder=$PWD
 
+# Byrja a ad setja upp allt fyrir serverinn og initialize-a SQL
 cd server
 bash install.sh
 cd $currentFolder
 
+# Installar ollu fyrir API
 cd API
 bash install.sh
+mvn package
 cd $currentFolder
 
-
-
+# Test keyra serverinn, endilega keyra sidan 'mvn package' 
 cd server/initial
 ./gradlew bootRun
 
