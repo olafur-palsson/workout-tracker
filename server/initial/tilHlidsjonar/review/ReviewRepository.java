76cea2b07d248b0d25520ec37f990cf484472665
olafur-palsson <olafur.palsson2@gmail.com>
2018-10-11 16:23:16 +0000

    Trimmin some mo'

--- server/initial/tilHlidsjonar/review/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-
-package server.routine;
-
-import org.springframework.data.repository.CrudRepository;
-
-// This will be AUTO IMPLEMENTED by Spring into a Bean called reviewRepository
-// // CRUD refers Create, Read, Update, Delete
-//
-public interface ReviewRepository extends CrudRepository<ReviewEntity, Long> { }
