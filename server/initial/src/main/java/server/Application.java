2630f09d5247ee633324fb5988f8742c5efdec8c
olafur-palsson <olafur.palsson2@gmail.com>
2018-11-19 23:31:20 +0000

    added cors allow on checklogin

--- server/initial/src/main/java/server/Application.java
+++ server/initial/src/main/java/server/Application.java
@@ -7,7 +7,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
 @EnableCaching
