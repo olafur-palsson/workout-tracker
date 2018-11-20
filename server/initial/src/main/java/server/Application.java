0fff269f4e0d642e4d3cd70605b8620640d8a390
olafur-palsson <olafur.palsson2@gmail.com>
2018-11-20 00:44:50 +0000

    yolo

--- server/initial/src/main/java/server/Application.java
+++ server/initial/src/main/java/server/Application.java
@@ -7,10 +7,14 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/*
 @ComponentScan
 @Configuration
 @EnableAutoConfiguration
 @EnableCaching
+*/
+
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
