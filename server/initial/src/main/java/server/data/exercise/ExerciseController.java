d5b469c2cd60780bfde45c055e27508b27065ab2
olafur-palsson <olafur.palsson2@gmail.com>
2018-12-14 20:11:21 +0000

    update

--- server/initial/src/main/java/server/data/exercise/ExerciseController.java
+++ server/initial/src/main/java/server/data/exercise/ExerciseController.java
@@ -13,46 +13,50 @@ import java.util.ArrayList;
 @RequestMapping(path="/database") // This means URL's start with /demo (after Application path)
 public class ExerciseController {
 
-    @Autowired
-	private ExerciseRepository exerciseRepository;
+  @Autowired
+  private ExerciseRepository exerciseRepository;
 
-    @CrossOrigin
-	@GetMapping(path = {"/addExercise", "/userEnabled/addExercise"})
-	public @ResponseBody
-	String addNewExercise(
-		 @RequestParam(required = false) Long id,
-         @RequestParam String name,
-         @RequestParam(required = false) ArrayList<String> description
-	) {
-		ExerciseEntity ee = new ExerciseEntity(name);
-		if(id != null) ee.setId(id);
-		if(description != null) ee.setDescription(description);
-		ee = exerciseRepository.save(ee);
-		return "" + ee.getId();
-	}
+  @CrossOrigin
+  @GetMapping(path = {"/addExercise", "/userEnabled/addExercise"})
+  public @ResponseBody String addNewExercise(
+  @RequestParam(required = false) Long id,
+  @RequestParam String name,
+  @RequestParam(required = false) ArrayList<String> description
+  ) {
+    ExerciseEntity ee = new ExerciseEntity(name);
+    if(id != null) ee.setId(id);
+    if(description != null) ee.setDescription(description);
+    ee = exerciseRepository.save(ee);
+    return "" + ee.getId();
+  }
 
-    @CrossOrigin
-	@GetMapping(path = {"/oneExercise", "/userEnabled/oneExercise"})
-	public @ResponseBody
-	ExerciseEntity getOneExercise(
-		@RequestParam Long id
-	) {
-		return exerciseRepository.findOne(id);
-	}
+  @CrossOrigin
+  @GetMapping(path = {"/oneExercise", "/userEnabled/oneExercise"})
+  public @ResponseBody ExerciseEntity getOneExercise(
+  @RequestParam Long id
+  ) {
+    return exerciseRepository.findOne(id);
+  }
 
-    @CrossOrigin
-    @GetMapping(path = {"/allExercises", "/userEnabled/allExercises"})
-    public @ResponseBody
-    Iterable<ExerciseEntity> getOneExercise( ) {
-        return exerciseRepository.findAll();
-    }
+  @CrossOrigin
+  @GetMapping(path = {"/allExercises", "/userEnabled/allExercises"})
+  public @ResponseBody Iterable<ExerciseEntity> getOneExercise( ) {
+    return exerciseRepository.findAll();
+  }
 
-    @CrossOrigin
-	@GetMapping(path = {"/removeExercise", "/userEnabled/removeExercise"})
-	public @ResponseBody String removeExercise(
-		 @RequestParam Long id
-	) {
-		exerciseRepository.delete(id);
-		return "Deleted";
-	}
+  @CrossOrigin
+  @GetMapping(path = {"/removeExercise", "/userEnabled/removeExercise"})
+  public @ResponseBody String removeExercise(
+  @RequestParam Long id
+  ) {
+    exerciseRepository.delete(id);
+    return "Deleted";
+  }
 }
+
+
+
+
+
+
+u
--- server/initial/src/main/java/server/data/exercise/ExerciseEntity.java
+++ server/initial/src/main/java/server/data/exercise/ExerciseEntity.java
@@ -1,9 +1,9 @@
 /**
- * Author: Olafur Palsson
- * HImail: olp6@gmail.com
- * Actual: olafur.palsson
- * Heiti verkefnis: server
- */
+* Author: Olafur Palsson
+* HImail: olp6@gmail.com
+* Actual: olafur.palsson
+* Heiti verkefnis: server
+*/
 
 package server.data.exercise;
 
@@ -12,46 +12,47 @@ import java.util.ArrayList;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class ExerciseEntity {
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-    long id;
-	private String name;
-	private String description;
-	private String imgURL;
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  long id;
+  private String name;
+  private String description;
+  private String imgURL;
 
-    public ExerciseEntity(String name) {
-	    this.name = name;
-    }
-    public ExerciseEntity() {
-        this.name = "No name";
-    }
+  public ExerciseEntity(String name) {
+    this.name = name;
+  }
+  public ExerciseEntity() {
+    this.name = "No name";
+  }
 
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+  /** Getters and setters ... */
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
 
-    public void setDescription(ArrayList<String> description) {
-        String desc = "";
-        for(String s : description)
-            desc += s;
-        this.description = desc;
-    }
+  public String getDescription() { return description; }
+  public void setDescription(String description) { this.description = description; }
 
-    public String getImgURL() {
-        return imgURL;
-    }
+  public void setDescription(ArrayList<String> description) {
+    String desc = "";
+    for(String s : description)
+    desc += s;
+    this.description = desc;
+  }
 
-    public void setImgURL(String imgURL) {
-        this.imgURL = imgURL;
-    }
+  public String getImgURL() {
+    return imgURL;
+  }
+
+  public void setImgURL(String imgURL) {
+    this.imgURL = imgURL;
+  }
 }
-
